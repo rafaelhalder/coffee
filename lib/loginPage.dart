defa7879ce1fa5b73acfce6593f3a99e6b5e86c9
Rafael Henrique da Silva <31450046+rafaelhalder@users.noreply.github.com>
2022-05-11 21:39:37 -0300

    init

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -42,19 +42,74 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
-            const TextInputField(
-                hint: 'Email',
-                icon: FontAwesomeIcons.envelope,
-                inputType: TextInputType.emailAddress,
-                inputAction: TextInputAction.next),
-            PasswordInputField(
-                hint: 'Password',
-                icon: FontAwesomeIcons.lock,
-                inputType: TextInputType.name,
-                inputAction: TextInputAction.done)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextInputField(
+                    hint: 'Email',
+                    icon: FontAwesomeIcons.envelope,
+                    inputType: TextInputType.emailAddress,
+                    inputAction: TextInputAction.next),
+                PasswordInputField(
+                    hint: 'Password',
+                    icon: FontAwesomeIcons.lock,
+                    inputType: TextInputType.name,
+                    inputAction: TextInputAction.done),
+                Text(
+                  'Forgot password',
+                  style: kBodyText,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                RoundedButton(
+                  buttonName: 'Login',
+                ),
+                SizedBox(
+                  height: 25,
+                )
+              ],
+            ),
+            Container(
+              child: Text(
+                'Create a new account',
+                style: kBodyText,
+              ),
+              decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(width: 1, color: kWhite))),
+            ),
+            SizedBox(
+              height: 20,
+            )
           ]),
         )
       ],
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton({
+    Key? key,
+    required this.buttonName,
+  }) : super(key: key);
+
+  final String buttonName;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.08,
+      width: size.width * 0.8,
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(16), color: kBlue),
+      child: TextButton(
+          onPressed: () {},
+          child: Text(
+            buttonName,
+            style: kBodyText.copyWith(fontWeight: FontWeight.bold),
+          )),
     );
   }
 }
